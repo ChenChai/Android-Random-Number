9f51c74160d334dcb742968c1f30f67694b11fbb
ChenChai <chen.chai@outlook.com>
2018-11-06 07:58:07 -0500

    Implemented countMe() to increment a textView

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.randomnumbergenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    // onClick functions must be public void, and have View as single argument
     public void toastMe(View view){
         Toast myToast = Toast.makeText(this, "Welcome Toast!", Toast.LENGTH_SHORT);
         myToast.show();
+        return;
+    }
+
+    public void countMe(View view){
+        TextView countTextView = (TextView) findViewById(R.id.textView);
+        String countString = countTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        countTextView.setText(count.toString());
         return;
     }
 
