009f53239d93f45c2569aa64400b9a4d6e12e094
ChenChai <chen.chai@outlook.com>
2018-11-06 07:52:52 -0500

    Implemented Toast function

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.randomnumbergenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState); // TODO: Write code
         setContentView(R.layout.activity_main);
     }
+
+    public void toastMe(View view){
+        Toast myToast = Toast.makeText(this, "Welcome Toast!", Toast.LENGTH_SHORT);
+        myToast.show();
+        return;
+    }
+
 }
