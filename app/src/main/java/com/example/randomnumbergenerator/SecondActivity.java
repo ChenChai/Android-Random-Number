719088e9fc98fda6c0b5947454405bf694e30a5c
ChenChai <chen.chai@outlook.com>
2018-11-07 08:44:57 -0500

    Implemented showRandomNumber() to display random number in second activity

--- app/src/main/java/com/example/randomnumbergenerator/SecondActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/SecondActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String TOTAL_COUNT = "total_count";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +18,22 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void showRandomNumber(){
+        Intent randomIntent = this.getIntent();
 
+        TextView randomTextView = (TextView) findViewById(R.id.random_textView);
+        TextView headerTextView = (TextView) findViewById(R.id.random_header_textView);
 
+        int count = randomIntent.getIntExtra(TOTAL_COUNT, 0);
 
+        Random random = new Random();
+
+        int randomInt = 0;
+        if(count > 0){
+            randomInt = random.nextInt(count);
+        }
+
+        randomTextView.setText(Integer.toString(randomInt));
+        headerTextView.setText(getString(R.string.random_heading,count));
 
     }
 
