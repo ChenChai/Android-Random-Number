397e449db8aaace7ba73c4cf688fc5a951592e41
ChenChai <chen.chai@outlook.com>
2018-11-07 08:39:47 -0500

    Added second activity and intent to start it

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.randomnumbergenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TOTAL_COUNT = "total_count";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // TODO: Write code
@@ -21,16 +21,23 @@ public class MainActivity extends AppCompatActivity {
     public void toastMe(View view){
         Toast myToast = Toast.makeText(this, "Welcome Toast!", Toast.LENGTH_SHORT);
         myToast.show();
-        return;
     }
 
     public void countMe(View view){
-        TextView countTextView = (TextView) findViewById(R.id.textView);
+        TextView countTextView = (TextView) findViewById(R.id.main_textView);
         String countString = countTextView.getText().toString();
         Integer count = Integer.parseInt(countString);
         count++;
         countTextView.setText(count.toString());
-        return;
     }
 
+    public void randomMe(View view){
+        Intent randomIntent = new Intent(this, SecondActivity.class);
+
+        TextView countTextView = (TextView) findViewById(R.id.main_textView);
+        Integer count = Integer.parseInt(countTextView.getText().toString());
+
+        randomIntent.putExtra(TOTAL_COUNT, count);
+        startActivity(randomIntent);
+    }
 }
---
+++ app/src/main/java/com/example/randomnumbergenerator/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.randomnumbergenerator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        showRandomNumber();
+    }
+
+    public void showRandomNumber(){
+
+
+
+
+    }
+
+}
